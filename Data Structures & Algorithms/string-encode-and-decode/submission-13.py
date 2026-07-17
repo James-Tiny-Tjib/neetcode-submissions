86475271ebd92677c59eafadecfceba8e9efbf62
James-Tiny-Tjib <168665817+James-Tiny-Tjib@users.noreply.github.com>
2026-07-17 17:08:39 -0400

    Add: string-encode-and-decode - submission-13

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-13.py
@@ -0,0 +1,41 @@
+class Solution:
+
+    # Schematic: num1_string1num2_string2 etc...
+    def encode(self, strs: List[str]) -> str:
+
+        encoded_str = ""
+
+        for s in strs:
+            encoded_str += str(len(s)) + "_" + s
+        
+        return encoded_str
+
+    def decode(self, s: str) -> List[str]:
+        # Decoded output
+        decoded_list = []
+
+        # i = index of the string
+        i = 0
+
+        # Iterate through the string
+        while i < len(s):
+
+            # Extract the length
+            len_list = []
+            while s[i] != "_":
+                len_list.append(s[i])
+                i+=1;
+
+            # Increment i for esc character
+            i+=1;
+
+            # Turn the 
+            length = int("".join(len_list))
+
+            decoded_list.append(s[i : i + length])
+
+            i += length
+
+        return decoded_list
+            
+
