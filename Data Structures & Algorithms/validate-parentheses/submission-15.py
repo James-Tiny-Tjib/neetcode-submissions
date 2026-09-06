99d1b56a44711515df0d2c316b40521df30fe37e
James-Tiny-Tjib <168665817+James-Tiny-Tjib@users.noreply.github.com>
2026-09-06 16:09:17 -0400

    Add: validate-parentheses - submission-15

---
+++ Data Structures & Algorithms/validate-parentheses/submission-15.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        # if the str is uneven, return False
+        if len(s) % 2 != 0:
+            return False
+
+        # Define Stack
+        stack = []
+        
+        # pair for parentheses
+        par_pair = {
+            ")" : "(",
+            "]" : "[",
+            "}" : "{",
+        }
+
+        # Iterate Through each paranthese
+        for c in s:
+            
+            if c in par_pair:
+                if stack and stack[-1] == par_pair[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+            
+        
+        return not stack
